0fac30f7778b1a4767059d1d9960338ab9c14e2f
NakamuraHayato <br343nh@yahoo.co.jp>
2020-05-24 21:51:10 +0900

    プレイヤーのジャンプ、床の移動実装

---
+++ Assets/Scripts/BlockManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockManager : MonoBehaviour
+{
+
+    private float speed; // ブロックが左に流れるスピード
+
+    void Start() {
+        speed = 0.08f;
+    }
+
+    void FixedUpdate() {
+        this.transform.Translate(-speed, 0, 0);
+
+        if (transform.position.x < -10) {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public LayerMask blockLayer; // ブロックレイヤー
+    private Rigidbody2D rbody; // プレイヤー制御用Rigidbody2D
+
+    private float jumpPower = 400;
+    private bool goJump = false; // ジャンプしたか否か
+    private bool canJump = false; // ジャンプ可能か否か
+
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        canJump =
+            Physics2D.Linecast(transform.position - (transform.right * 0.3f),
+                transform.position - (transform.up * 0.5f), blockLayer) ||
+            Physics2D.Linecast(transform.position + (transform.right * 0.3f),
+                transform.position - (transform.up * 0.5f), blockLayer);
+
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown("space")) { // 画面クリックかスペースキーでジャンプ
+            PushJumpButton();
+        }
+
+        if (transform.position.y < -8) {
+            GameOver();
+        }
+    }
+
+    void FixedUpdate()
+    {
+
+        if (goJump) {
+            rbody.AddForce(Vector2.up * jumpPower);
+            goJump = false;
+        }
+    }
+
+    // ジャンプボタンが押された時
+    public void PushJumpButton() {
+        if (canJump) {
+            goJump = true;
+        }
+    }
+
+    // ゲームオーバー
+    public void GameOver() {
+        Destroy(gameObject);
+    }
+}
