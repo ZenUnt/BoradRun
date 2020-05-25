333a05b9a7c9b190fffc75cbb9374f15d0fcd2e4
NakamuraHayato <br343nh@yahoo.co.jp>
2020-05-25 23:40:20 +0900

    小ジャンプ実装

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
 
 public class PlayerManager : MonoBehaviour
 {
-    public LayerMask blockLayer; // ブロックレイヤー
-    private Rigidbody2D rbody; // プレイヤー制御用Rigidbody2D
+    // 定数定義
+    private const int JUMP_BORDER_TIME = 180; // 小ジャンプと大ジャンプの境界のミリ秒数
 
-    private float jumpPower = 400;
-    private bool goJump = false; // ジャンプしたか否か
-    private bool canJump = false; // ジャンプ可能か否か
+    public LayerMask blockLayer;    // ブロックレイヤー
+    private Rigidbody2D rbody;      // プレイヤー制御用Rigidbody2D
+
+    private float jumpPower = 300;   // ジャンプパワー
+
+    private bool canJump = false;   // ジャンプ可能か否か
+    private bool goJump = false;    // ジャンプしたか否か
+    private DateTime mouseDownTime; // 画面がクリックされた時刻
+    private TimeSpan spanMouseDownUp; // マウスがクリックされてから離されるまでの時間
 
     void Start() {
         rbody = GetComponent<Rigidbody2D>();
@@ -23,7 +31,16 @@ public class PlayerManager : MonoBehaviour
                 transform.position - (transform.up * 0.5f), blockLayer);
 
         if (Input.GetMouseButtonDown(0) || Input.GetKeyDown("space")) { // 画面クリックかスペースキーでジャンプ
+            mouseDownTime = DateTime.UtcNow;
             PushJumpButton();
+        }
+
+        // 押す時間が短ければ小ジャンプ
+        if (Input.GetMouseButtonUp(0)) {
+            spanMouseDownUp = DateTime.UtcNow - mouseDownTime;
+            if (spanMouseDownUp < TimeSpan.FromMilliseconds(JUMP_BORDER_TIME)) {
+                rbody.AddForce(Vector2.down * jumpPower * 0.4f);
+            }
         }
 
         if (transform.position.y < -8) {
@@ -49,5 +66,6 @@ public class PlayerManager : MonoBehaviour
     // ゲームオーバー
     public void GameOver() {
         Destroy(gameObject);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
